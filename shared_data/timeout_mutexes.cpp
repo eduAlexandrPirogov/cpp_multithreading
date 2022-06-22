1cfbcb25e9e0e1d71a4fdb687a61fd45b60f99f1
eduAlexandrPirogov <edualexandrpirogov@gmail.com>
2022-06-22 13:05:17 -0500

    fix src

--- shared_data/timeout_mutexes.cpp
+++ shared_data/timeout_mutexes.cpp
@@ -33,14 +33,33 @@ void task2()
 
    m.unlock();
    std::cout << "Task2 finally did it...\n";
+   std::this_thread::sleep_for(1000ms);
+};
+
+void task3()
+{
+    std::this_thread::sleep_for(1000ms);
+    std::cout << "trying to lock mutex from task3...\n";
+
+    auto deadline = std::chrono::system_clock::now() + 1000ms;
+    while(!m.try_lock_until(deadline))
+    {
+        std::cout << "Can't lock mutex from task3...\n";
+	std::this_thread::sleep_for(1000ms);
+    };
+
+    std::cout << "I took a mutex from task3!\n";
+    m.unlock();
 };
 
 int main()
 {
    std::thread t1{task1};
    std::thread t2{task2};
+   std::thread t3{task3};
 
    t1.join();
    t2.join();
+   t3.join();
    return 0;
 };
