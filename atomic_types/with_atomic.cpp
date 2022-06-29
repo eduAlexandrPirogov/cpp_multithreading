b303437f484191b859939aba1a8278ff16dfe0c6
eduAlexandrPirogov <eduAlexandrPirogov@gmail.com>
2022-06-29 17:14:53 +0300

    added src

---
+++ atomic_types/with_atomic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <atomic>
+
+std::atomic<int> count {0};
+
+void task()
+{
+   for(int i = 0 ;i < 10000000; i++)
+	   ++count;
+};
+
+int main()
+{
+   std::vector<std::thread> vec;
+   for(int i = 0; i < 10; i++)
+	   vec.push_back(std::thread{task});
+   for(auto& t : vec)
+	   t.join();
+   std::cout << count << '\n';
+};
